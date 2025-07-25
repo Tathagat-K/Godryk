eb72f02426f40289451e9c630d44448e67fccc76
Tathagat-K <tathagat.k@netsmartz.com>
2025-07-25 15:52:44 +0530

    Include base router

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 # src/main.py
 
 from fastapi import FastAPI
+from src.api.routes import router
 from src.api.routes.ingest_router import router as ingest_router
 
 app = FastAPI(title="RAG FastAPI")
 
+app.include_router(router)
 app.include_router(ingest_router)
 
 @app.get("/")
